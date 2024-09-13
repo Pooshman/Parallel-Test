5879634e37255bb208eb33adabc04069e4e0ed0f
Purush Ram <purushnram@gmail.com>
2024-09-12 19:21:53 -0700

    Update run_snakemake.py

--- run_snakemake.py
+++ run_snakemake.py
@@ -1,20 +1,15 @@
-# run_snakemake.py
-
 import subprocess
 
-# Define Snakemake command
-snakemake_cmd = [
+# Define Snakemake command with cluster support
+cmd = [
     "snakemake",
     "--snakefile", "Snakefile",
-    "--cores", "16",  # Total number of cores to be used
+    "--jobs", "5",  # Max parallel jobs
     "--cluster-config", "cluster.yaml",
-    "--cluster", "sbatch --nodes={nodes} --ntasks-per-node={ntasks-per-node} --cpus-per-task={cpus-per-task} --mem={mem} --time={time}",
-    "--jobs", "100",  # Maximum number of jobs to be submitted at once
-    "--use-conda",
-    "--rerun-incomplete",
+    "--cluster", "sbatch --nodes={cluster.nodes} --ntasks-per-node={cluster.ntasks-per-node} --cpus-per-task={cluster.cpus-per-task} --mem={cluster.mem} --time={cluster.time} --output=output_%j.txt",
     "--keep-going",
     "--latency-wait", "60"
 ]
 
-# Run Snakemake command
-subprocess.run(snakemake_cmd, check=True)
+# Execute the Snakemake command
+subprocess.run(cmd)
