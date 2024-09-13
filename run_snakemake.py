8526d0054cc284db340aa4ca78c03f333be1349c
Purush Ram <purushnram@gmail.com>
2024-09-12 19:24:08 -0700

    Update run_snakemake.py

--- run_snakemake.py
+++ run_snakemake.py
@@ -6,7 +6,11 @@ cmd = [
     "--snakefile", "Snakefile",
     "--jobs", "5",  # Max parallel jobs
     "--cluster-config", "cluster.yaml",
-    "--cluster", "sbatch --nodes={cluster.nodes} --ntasks-per-node={cluster.ntasks-per-node} --cpus-per-task={cluster.cpus-per-task} --mem={cluster.mem} --time={cluster.time} --output=output_%j.txt",
+    "--cluster", (
+        "sbatch --nodes={cluster.nodes} --ntasks-per-node={cluster.ntasks-per-node} "
+        "--cpus-per-task={cluster.cpus-per-task} --mem={cluster.mem} --time={cluster.time} "
+        "--output={cluster.output}"
+    ),
     "--keep-going",
     "--latency-wait", "60"
 ]
